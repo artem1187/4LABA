d95c08963fc9b62df7ee603b0b794d8619e78f87
artem1187 <bukinartem11@gmail.com>
2025-12-02 15:59:33 +0700

    start project

---
+++ main.py
@@ -0,0 +1,11 @@
+
+# ================================================================
+# 1. Импорты и проверка устройства
+# ================================================================
+
+import torch, torchvision, time, onnx, onnxruntime, numpy as np
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Device:", device)
